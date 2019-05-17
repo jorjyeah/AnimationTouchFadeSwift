f4715f865ba133284d6d92ef5f47eaf911240e41
George Joseph Kristian <jorji.gjk@gmail.com>
2019-05-17 16:32:45 +0700

    Animation with damping

--- AnimationTouchFade/ViewController.swift
+++ AnimationTouchFade/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var target: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        target.layer.cornerRadius = target.frame.size.width/2
+        target.clipsToBounds = true
     }
+    
+    @IBAction func targetTapped(_ gestureRecognizer : UITapGestureRecognizer) {
+        if gestureRecognizer.state == .ended {
+            // move random to x, y
+            target.moveTo(x: CGFloat(Int.random(in: 0 ... 414)), y: CGFloat(Int.random(in: 0 ... 896)), duration: 1)
+        }
+    }
+}
 
-
+extension UIView {
+    func moveTo(x: CGFloat, y: CGFloat, duration: Double) {
+        UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+            self.center.x = x
+            self.center.y = y
+            self.backgroundColor = self.generateRandomColor()
+        })
+    }
+    
+    func generateRandomColor() -> UIColor {
+        let hue : CGFloat = CGFloat(arc4random() % 256) / 256 // use 256 to get full range from 0.0 to 1.0
+        let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from white
+        let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from black
+        
+        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+    }
 }
 
